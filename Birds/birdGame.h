e950016e8ac87e84747bce54f2f33b934120b703
bmitch201 <33267060+bmitch201@users.noreply.github.com>
2017-11-27 23:15:24 +0000

    Add project files.

---
+++ Birds/birdGame.h
@@ -0,0 +1,15 @@
+#ifndef _BIRDSGAME_H
+#define _BIRDSGAME_H
+
+// Card game headers
+#include "GameConstants.h"
+#include "cSprite.h"
+#include "cBkGround.h"
+#include "cTextureMgr.h"
+#include "cTexture.h"
+#include "cBird.h"
+#include "cPlane.h"
+#include "cFontMgr.h"
+#include "cSoundMgr.h"
+
+#endif
